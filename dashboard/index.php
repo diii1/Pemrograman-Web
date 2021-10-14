55b6634b5c64aafded1b9c12777ca5ce85832965
Bayu <bayu.setiaji709@gmail.com>
2021-10-14 20:59:44 +0700

    creating-dashboard

---
+++ dashboard/index.php
@@ -0,0 +1,153 @@
+<?php
+    include('conn.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Profile - Admin Dashboard</title>
+    
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    
+    <link rel="stylesheet" href="assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
+    <link rel="stylesheet" href="assets/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon">
+</head>
+
+<body>
+    <div id="app">
+        <div id="sidebar" class="active">
+            <div class="sidebar-wrapper active">
+                <div class="sidebar-header">
+                    <div class="d-flex justify-content-between">
+                        <div class="logo">
+                            <a href="index.html"><img src="assets/images/logo/logo.png" alt="Logo" srcset=""></a>
+                        </div>
+                        <div class="toggler">
+                            <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="sidebar-menu">
+                    <ul class="menu">
+                        <li class="sidebar-title">Data Profile Mahasiswa</li>
+                        
+                        <li
+                            class="sidebar-item active ">
+                            <a href="table.html" class='sidebar-link'>
+                                <i class="bi bi-grid-1x2-fill"></i>
+                                <span>Biodata</span>
+                            </a>
+                        </li>
+                        
+                    </ul>
+                </div>
+                <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+            </div>
+        </div>
+        <div id="main">
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
+            
+            <div class="page-heading">
+                <div class="page-title">
+                    <div class="row">
+                        <div class="col-12 col-md-6 order-md-1 order-last">
+                            <h3>Biodata Mahasiswa</h3>
+                            <!-- <p class="text-subtitle text-muted">For user to check they list</p> -->
+                        </div>
+                        <div class="col-12 col-md-6 order-md-2 order-first">
+                            <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Table Biodata</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Table head options start -->
+                <section class="section">
+                    <div class="row" id="table-head">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="card-title">Table Biodata Mahasiswa</h4>
+                                    <div><a href="biodata/insert.php" class="btn btn-success">Tambah Data</a></div>
+                                </div>
+                                <div class="card-content">
+                                    <!-- table head dark -->
+                                    <div class="table-responsive">
+                                        <table class="table mb-0">
+                                            <thead class="thead-dark">
+                                                <tr>
+                                                    <th>Link Profile</th>
+                                                    <th>Nama</th>
+                                                    <th>Nama Panggilan</th>
+                                                    <th>Email</th>
+                                                    <th>Instagram</th>
+                                                    <th>Alamat</th>
+                                                    <th>Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php 
+                                                    $query = "SELECT * FROM biodata";
+                                                    $result = mysqli_query(connection(),$query);
+                                                ?>
+                                                <?php while($data = mysqli_fetch_array($result)): ?>
+                                                    <tr>
+                                                        <td><?php echo $data['profil']; ?></td>
+                                                        <td><?php echo $data['name']; ?></td>
+                                                        <td><?php echo $data['short_name']; ?></td>
+                                                        <td><?php echo $data['email']; ?></td>
+                                                        <td><?php echo $data['instagram']; ?></td>
+                                                        <td><?php echo $data['alamat']; ?></td>
+                                                        <td>
+                                                            <a href="#" class="btn btn-warning">Update</a>
+                                                            <a href="#" class="btn btn-danger">Delete</a>
+                                                        </td>
+                                                    </tr>
+                                                <?php endwhile ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <!-- Table head options end -->
+
+            </div>
+
+            <footer>
+                <div class="footer clearfix mb-0 text-muted">
+                    <div class="float-start">
+                        <p>2021 &copy; Mazer</p>
+                    </div>
+                    <div class="float-end">
+                        <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
+                                href="http://ahmadsaugi.com">A. Saugi</a></p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+
+    <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/mazer.js"></script>
+</body>
+
+</html>
