6678e8e65881546cc8869ac0b1271689283c1d33
Bayu <bayu.setiaji709@gmail.com>
2021-10-16 15:42:59 +0700

    fix-link

--- dashboard/skill.php
+++ dashboard/skill.php
@@ -46,14 +46,28 @@
                 <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                     <div class="sb-sidenav-menu">
                         <div class="nav">
+                            <div class="sb-sidenav-menu-heading">BIODATA</div>
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#biodata" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Biodata Table
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="biodata" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="<?php echo 'biodata.php';?>">View Biodata</a>
+                                    <a class="nav-link" href="layout-static.html">Add Biodata</a>
+                                    <a class="nav-link" href="layout-static.html">Update Biodata</a>
+                                </nav>
+                            </div>
                             <div class="sb-sidenav-menu-heading">SKILL</div>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#skill" aria-expanded="false" aria-controls="collapseLayouts">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                                 Skill Table
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                            <div class="collapse" id="skill" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link active" href="<?php echo 'skill.php';?>">View Skill</a>
                                     <a class="nav-link" href="layout-static.html">Add Skill</a>
                                     <a class="nav-link" href="layout-static.html">Update Skill</a>
                                 </nav>
